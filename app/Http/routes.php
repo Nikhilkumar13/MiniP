e6a2c7308575fcd96cc567652e00168c93053059
Nikhil Kumar <nikkr007@gmail.com>
2016-03-08 22:39:17 +0530

    before adding graph

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -41,6 +41,15 @@ Route::get('/try', function () {
 |
 */
 
-Route::group(['middleware' => ['web']], function () {
-    //
+// Route::group(['middleware' => ['web']], function () {
+//     //
+// });
+
+Route::group(['middleware' => 'web'], function () {
+	Route::get('/', function () {
+    return view('welcome');
+});
+    Route::auth();
+
+    Route::get('/home', 'HomeController@index');
 });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,58 +8,53 @@
   <!-- CSS  -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <link href="{{asset('/css/materialize.min.css')}}" type="text/css" rel="stylesheet" media="screen,projection"/>
-<!--   <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <script src="https://code.highcharts.com"></script>
+    <!-- <link href="{{asset('/css/extras/nouislider.min.css')}}" type="text/css" rel="stylesheet" media="screen,projection"/> -->
+    <!-- <link href="{{asset('/css/nouislider.tooltips.css')}}" type="text/css" rel="stylesheet" media="screen,projection"/> -->
+    <!-- <link href="{{asset('/css/nouislider.pips.css')}}" type="text/css" rel="stylesheet" media="screen,projection"/> -->
 
- --></head>
+
+</head>
 <body style="overflow:hidden">
-  <nav class="light-blue lighten-1" role="navigation">
-<!--     <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Logo</a>
- -->     
 
-       <ul class="right hide-on-med-and-down">
-        <li><a class="modal-trigger waves-effect waves-light btn" href="#signup">Sign Up</a></li>
-        <li><a class="modal-trigger waves-effect waves-light btn" href="#login">Login</a></li>
-        <!-- <li><a href="#">Sign In</a></li> -->
-
-      </ul>
-
-      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
-    </div>
-  </nav>
-
-
-  <!-- <ul id="slide-out" class="side-nav fixed ">
-    <li><a href="#!">First Sidebar Link</a></li>
-    <li><a href="#!">Second Sidebar Link</a></li>
-  </ul> -->
 
 
   <div   style="width:15%;float:left;overflow:hidden;height:20%;overflow-x:hidden ">
  
-        <!-- <a href="#" data-activates="slide-out" class="button-collapse"><i class="mdi-navigation-menu"></i></a> -->
 
-  <ul  id= "menu" class="collapsible" data-collapsible="accordion" style=" margin-top:-1%;margin-right:-8%;">
+   <ul  id= "menu" class="collapsible" data-collapsible="accordion" style=" margin-top:-1%;margin-right:-8%; ">
     <li>
       <div class="collapsible-header"><i class="material-icons">filter_drama</i>Dengue </div>
       <div class="collapsible-body">
         <ul>
           <li class="collapsible-header" style="margin-left:40px"><a type="button"  id ="reloadMap"><i class="material-icons">places</i>Show Map</a></li>
-          <li class="collapsible-header" style="margin-left:35px" ><a type="button"  id ="reloadMap"><i class="material-icons">trending_up</i>Graphs</a></li>
+          <li class="collapsible-header" style="margin-left:35px" ><a type="button"  id ="reloadGraph"><i class="material-icons">trending_up</i>Graphs</a></li>
           <li class="collapsible-header" style="margin-left:35px" ><a type="button"  id ="reloadMap"><i class="material-icons">trending_up</i>Heat Maps</a></li>
-          <li class="collapsible-header" style="margin-left:35px" ><a type="button"  id ="register"><i class="material-icons">trending_up</i>Register</a></li>
         </ul>
       </div>
     </li>
     <li>
       <div class="collapsible-header"><i class="material-icons">place</i>Dog Bites</div>
-      <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+      <div class="collapsible-body"><ul>
+          <li class="collapsible-header" style="margin-left:40px"><a type="button"  id ="reloadMap"><i class="material-icons">places</i>Show Map</a></li>
+          <li class="collapsible-header" style="margin-left:35px" ><a type="button"  id ="reloadMap"><i class="material-icons">trending_up</i>Graphs</a></li>
+          <li class="collapsible-header" style="margin-left:35px" ><a type="button"  id ="reloadMap"><i class="material-icons">trending_up</i>Heat Maps</a></li>
+        </ul>
+      </div>
     </li>
      <li>
-      <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
-      <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+      <div class="collapsible-header"><a style="color:black"   onclick="register(); return false;" ><i style="color:black" class="material-icons">receipt</i>Report Incident</a></div>
     </li>
 
   </ul>
+
+
+ <!--  <div id="showmapfilter" class="row" >
+    <div id="slider" class="row s12 pading" style="margin-top:40px">
+
+    </div>
+  </div> -->
+
 
   <div id ="registerform" class="row" style="display:none">
     <form class="col s12">
@@ -80,13 +75,13 @@
         </div>
         
       </div>
-      <div class="row">
+      <!-- <div class="row">
         <div class="input-field col s12">
           <input placeholder="radius" id="radius" type="text" class="validate">
           <label for="radius">Radius</label>
         </div>
         
-      </div>
+      </div> -->
        <div class="row">
         <div class="input-field col s12">
           <input placeholder="lat" id="lat" type="text" class="validate">
@@ -102,32 +97,12 @@
         
       </div>
       <div class="row">
+
         <button type="button" onclick="saveData(); return false;" class="waves-effect waves-light btn">Save</button>
+        <button type="button" onclick="back(); return false;" class="waves-effect waves-light btn">Back</button>
+
       </div>
 
-        <!-- Modal Trigger -->
-  
-
-
-
-      <!--<div class="row">
-        <div class="input-field col s12">
-          <input disabled value="I am not editable" id="disabled" type="text" class="validate">
-          <label for="disabled">Disabled</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input id="password" type="password" class="validate">
-          <label for="password">Password</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input id="email" type="email" class="validate">
-          <label for="email">Email</label>
-        </div>
-      </div> -->
     </form>
   </div>
         
@@ -136,31 +111,32 @@
   <!-- signup Modal Structure -->
   <div id="signup" class="modal modal-fixed-footer" style="width: 40%;height: 61%;">
     <div class="modal-content" style="height: 100%;">
-      <form class="col s12">
+      <form role="form " class="col s12">
+        <input type="hidden" id="token" name="_token" value="<?php echo csrf_token(); ?>">
         <h3>Sign Up</h3>
         <div class="row">
           <div class="input-field col s6">
-            <input  id="first_name" type="text" class="validate">
-            <label for="first_name">First Name</label>
+            <input  id="name" type="text" class="validate">
+            <label for="name">Name</label>
           </div>
-          <div class="input-field col s6">
-            <input  id="last_name" type="text" class="validate">
-            <label for="last_name">Last Name</label>
-          </div>
-        </div>
-      <div class="row">
+           <div class="row">
         <div class="input-field col s6">
           <input id="email" type="email" class="validate">
           <label for="email">Email</label>
         </div>
+     
         <div class="input-field col s6">
           <input id="password" type="password" class="validate">
           <label for="password">Password</label>
         </div>
+        <div class="input-field col s6">
+          <input id="confirmpassword" type="password" class="validate">
+          <label for="confirm password">Confirm Password</label>
+        </div>
       </div>
       <div class="modal-footer" style="position:inherit;">
           <div class="input-field">
-              <button class="modal-action modal-close waves-effect waves-light btn" onclick="">SignUP</button>
+              <button type= "button" class="modal-action waves-effect waves-light btn" onclick="signUp(); return false;">SignUP</button>
               <button class="waves-effect waves-light btn-flat" value="Reset" type="reset">Reset</button>
             <!-- <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat ">Agree</a> -->
         </div>
@@ -168,21 +144,12 @@
       </form>
     </div>
   </div>
+</div>
   <!-- login Modal Structure -->
   <div id="login" class="modal modal-fixed-footer" style="height: 61%;width: 40%;">
     <div class="modal-content">
       <form class="col s12">
         <h3>Login</h3>
-<!--         <div class="row">
-          <div class="input-field col s6">
-            <input placeholder="Enter First name" id="first_name" type="text" class="validate">
-            <label for="first_name">First Name</label>
-          </div>
-          <div class="input-field col s6">
-            <input placeholder="Enter last name" id="last_name" type="text" class="validate">
-            <label for="last_name">Last Name</label>
-          </div>
-        </div> -->
       <div class="row">
         <div class="input-field col s12">
           <input id="email" type="email" class="validate">
@@ -203,6 +170,8 @@
       </form>
     </div>
   </div>
+  <div id="graph" style="height: 400px; min-width: 310px; display:none"></div>
+
       
 
   <div  style="width: 1100px; height: 550px; float:right; margin-right:0px; margin-bottom:0px; " class="container" id ="map" >
@@ -216,12 +185,16 @@ var markers=[];
 var image= "{{asset('/images/marker.png')}}";
 
 function initMap() {
-  var myLatLng = {lat: -34.397, lng: 150.644};
+  var myLatLng = {lat: 28.54539, lng: 77.188181};
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 16,
-    center: myLatLng,
+    disableDefaultUI:true,
+    center:myLatLng,
+    zoomControl:true,
     rotateControl:true,
   });
+  // map.setTilt(45);
+  // map.setHeading(map.getHeading()||0+45);
 
    marker = new google.maps.Marker({
     position: myLatLng,
@@ -230,7 +203,7 @@ function initMap() {
     draggable: true,
     icon:image 
   });
-  getLocation();
+  // getLocation();
 }
 
 function getLocation() {
@@ -296,12 +269,20 @@ $(document).ready(function()
 
 
 });
+        $("#reloadGraph").on('click', function() {
+        // console.log('cbekvbhwv');
+        showGraph();
+    
 
-      $("#register").on('click',function()
-      {
-        register();
 
-      });
+
+});
+
+      // $("#register").on('click',function()
+      // {
+      //   register();
+
+      // });
 
 
 });
@@ -310,9 +291,44 @@ $(document).ready(function()
   <script src="{{asset('/js/materialize.js')}}"></script>
   <script src="{{asset('js/init.js')}}"></script>
 <script src="{{asset('js/locationpicker.jquery.js')}}"></script>
+<script src="{{asset('js/nouislider.min.js')}}"></script>
+
 
 
 <script language="javascript" type="text/javascript">
+
+
+
+
+var range = document.getElementById('slider');
+
+noUiSlider.create(range, {
+  start: [ 20, 80 ], // Handle start position
+  step: 10, // Slider moves in increments of '10'
+  margin: 20, // Handles must be more than '20' apart
+  connect: true, // Display a colored bar between the handles
+  direction: 'rtl', // Put '0' at the bottom of the slider
+  orientation: 'horizontal', // Orient the slider vertically
+  behaviour: 'tap-drag', // Move handle on tap, bar is draggable
+  range: { // Slider can select '0' to '100'
+    'min': 0,
+    'max': 100
+  },
+  pips: { // Show a scale with the slider
+    mode: 'steps',
+    density: 2
+  }
+});
+
+function showGraph()
+{
+        document.getElementById("map").style.display = "none";
+        document.getElementById("graph").style.display = "";
+
+
+}
+
+
   function getData(type) {
       $.ajax({
         url: '/data/',
@@ -321,6 +337,7 @@ $(document).ready(function()
   success: function(data) {
   console.log(data);
   setData(data);
+  // setMapOnAll(map);
   },
   error: function(e) {
   //called when there is an error
@@ -357,26 +374,94 @@ $(document).ready(function() {
   });
 
 
+function back()
+{
+      document.getElementById("registerform").style.display = "none";
+      document.getElementById("menu").style.display = "";
+
+}
 
   function register()
   {
 
-    setMapOnAll(null);
+    // setMapOnAll(null);
+     for(var i=0; i <markers.length;i++)
+     {
+      markers[i].setMap(null);
+     }
 
     document.getElementById("menu").style.display = "none";
     document.getElementById("registerform").style.display = "";
- $('#map').locationpicker({
-  location: {latitude: 28.549013499998987, longitude: 77.18317139999999}, 
-  radius: 10,
-  inputBinding: {
-        latitudeInput: $('#lat'),
-        longitudeInput: $('#lng'),
-        radiusInput: $('#radius'),
-        locationNameInput: $('#location')
+
+ // $('#map').locationpicker({
+ //  location: {latitude: 28.549013499998987, longitude: 77.18317139999999}, 
+ //  radius: 10,
+ //  inputBinding: {
+ //        latitudeInput: $('#lat'),
+ //        longitudeInput: $('#lng'),
+ //        radiusInput: $('#radius'),
+ //        locationNameInput: $('#location')
+ //    }
+ //  });
+  google.maps.event.addListener(map, 'click', function(event) {                
+        //Get the location that the user clicked.
+        var clickedLocation = event.latLng;
+        console.log(event);
+        //If the marker hasn't been added.
+        if(marker === false){
+            //Create the marker.
+            marker = new google.maps.Marker({
+                position: clickedLocation,
+                map: map,
+                draggable: true //make it draggable
+            });
+            //Listen for drag events!
+            google.maps.event.addListener(marker, 'dragend', function(event){
+                markerLocation();
+            });
+        } else{
+            //Marker has already been added, so just change its location.
+            console.log("yo");
+            marker.setPosition(clickedLocation);
+        }
+
+
+//         var circle = new google.maps.Circle({
+//   map: map,
+//   radius: 5,    // 10 miles in metres
+//   fillColor: '#AA0000'
+// });
+// circle.bindTo('center', marker, 'position');
+
+
+        //Get the marker's location.
+        markerLocation();
+    });
+}
+
+function markerLocation(){
+    //Get location.
+    var currentLocation = marker.getPosition();
+    //Add lat and lng values to a field that we can save.
+    document.getElementById('lat').value = currentLocation.lat(); //latitude
+    document.getElementById('lng').value = currentLocation.lng(); //longitude
+    // document.getElementById('location').value = marker.getPlace.name; //longitude
+
+    var geocoder = new google.maps.Geocoder();
+  geocoder.geocode({
+    latLng: currentLocation
+  }, function(responses) {
+    if (responses && responses.length > 0) {
+      document.getElementById('location').value=responses[0].formatted_address;
+      console.log(responses);
+
+    } else {
+     document.getElementById('location').value='Unknown location.';
     }
   });
 
-  }
+}
+
   function saveData() {
 
       // event.preventDefault();
@@ -405,6 +490,42 @@ $(document).ready(function() {
 
    });
   }
+
+  function signUp()
+  {
+
+      var formData={
+                'name' : $('#name').val(),
+                'email':$('#email').val(),
+                'password':$('#password').val(),
+                'password_confirmation':$('#confirmpassword').val(),
+                '_token':$('#token').val()
+                };
+
+
+                 $.ajax({
+  url: '/register/',
+  type: 'POST',
+  data: formData,
+  success: function(data) {
+
+    Materialize.toast('Signed In Succesfully', 4000,'rounded')
+    $('#signup').closeModal();
+
+  },
+  error: function(e) {
+  //called when there is an error
+  console.log(e);
+  alert('data not added, !!');
+  }
+
+   });
+
+
+    console.log(formData);
+
+
+  };
  
    </script>
   
@@ -412,5 +533,8 @@ $(document).ready(function() {
         $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
+    $(".button-collapse").sideNav();
+    // $('.collapsible').collapsible();
   });
+
 </script>
