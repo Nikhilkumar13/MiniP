f800c8fd9fc0eeb5e41495023d7c36872a5f47f2
Nikhil Kumar <nikkr007@gmail.com>
2016-04-12 22:35:39 +0530

    improved UI icons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,7 @@
 
       <!-- CSS  -->
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
       <link href="{{asset('/css/materialize.min.css')}}" type="text/css" rel="stylesheet" media="screen,projection"/>
       <link href="{{asset('/css/pikaday.css')}}" type="text/css" rel="stylesheet" media="screen,projection"/>
 
@@ -33,26 +34,26 @@
         <div class="col s2 " >
          <ul  id= "menu" class="collapsible" data-collapsible="accordion">
           <li>
-            <div class="collapsible-header"><i class="material-icons">filter_drama</i>Dengue </div>
+            <div class="collapsible-header active"><i class="material-icons">bug_report</i>Dengue Cases </div>
             <div class="collapsible-body">
               <ul>
                 <li class="collapsible-header" style="margin-left:40px"><a type="button"  id ="reloadMap1"><i class="material-icons">places</i>Show Map</a></li>
                 <li class="collapsible-header" style="margin-left:35px" ><a type="button"  id ="reloadGraph1"><i class="material-icons">trending_up</i>Graphs</a></li>
-                <li class="collapsible-header" style="margin-left:35px" ><a type="button"  id ="reloadHeatMap1"><i class="material-icons">trending_up</i>Heat Maps</a></li>
+                <li class="collapsible-header" style="margin-left:35px" ><a type="button"  id ="reloadHeatMap1"><i class="material-icons">layers</i>Heat Maps</a></li>
               </ul>
             </div>
           </li>
           <li>
-            <div class="collapsible-header"><i class="material-icons">place</i>Dog Bites</div>
+            <div class="collapsible-header"><i class="material-icons">pets</i>Dog Bites</div>
             <div class="collapsible-body"><ul>
               <li class="collapsible-header" style="margin-left:40px"><a type="button"  id ="reloadMap2"><i class="material-icons">places</i>Show Map</a></li>
               <li class="collapsible-header" style="margin-left:35px" ><a type="button"  id ="reloadGraph2"><i class="material-icons">trending_up</i>Graphs</a></li>
-              <li class="collapsible-header" style="margin-left:35px" ><a type="button"  id ="reloadHeatMap2"><i class="material-icons">trending_up</i>Heat Maps</a></li>
+              <li class="collapsible-header" style="margin-left:35px" ><a type="button"  id ="reloadHeatMap2"><i class="material-icons">layers</i>Heat Maps</a></li>
             </ul>
           </div>
         </li>
         <li>
-          <div class="collapsible-header"><a style="color:black"   onclick="register(); return false;" ><i style="color:black" class="material-icons">receipt</i>Report Incident</a></div>
+          <div class="collapsible-header"><a style="color:black"   onclick="register(); return false;" ><i style="color:black" class="material-icons">add_location</i>Report Incident</a></div>
         </li>
 
       </ul>
@@ -64,189 +65,190 @@
     <div id ="registerform" class="col s2" style="display:none;">
       <form class="formValidate" id="formdata">
 
+        <div class="row">
+          <div class="input-field col s12">
+            <input  data-error="User Id required! "name="uid" placeholder="User Id" id="uid" type="text" class="validate truncate">
+            <label for="uid">User Id</label>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="input-field col s12">
+            <input placeholder="Location" id="location" type="text" class="validate truncate">
+            <label for="Location">Location</label>
+          </div>
+        </div>
+
+
+        <div class="row">
+          <div class="input-field col s6 ">
+            <input placeholder="lat" id="lat" type="text" class="validate truncate" >
+
+            <label for="lat">Lat</label>
+          </div>
+          <div class="input-field col s6">
+            <input placeholder="lng" id="lng" type="text" class="validate truncate">
+            <label for="lng">Long</label>
+
+
+          </div>
+
+
+        </div>
+
+
+        <div class="row" >
+          <div class="col s6">
+            <input name="typegroup" type="radio"  value ="mosquito"  id="test1" checked />
+
+            <label for="test1">Mosquito</label>
+          </div>
+
+          <div class="col s6">
+            <input name="typegroup"  type="radio" value ="dog"  id="test2" />
+
+            <label for="test2">Dog</label>
+          </div>
+
+
+        </div>
+
+        <div class="row">
+
+
+          <div class="col s6  blue-text  ">
+           <p style="font-size:17px">Date:</p>
+         </div>
+         <div class="col s6   blue-text text-darken-2 no-padding">
+          <input placeholder="idate" type="date" class="datepicker tooltipped center-align" data-position="bottom" data-delay="50" data-tooltip="Click here to input date of the day you got bitten" value="<?php echo date("d-M-Y"); ?>" id="idate">
+        </div>
+
+
+
+      </div>
+
+
+
+
+
       <div class="row">
-        <div class="input-field col s12">
-          <input  data-error="User Id required! "name="uid" placeholder="User Id" id="uid" type="text" class="validate truncate">
-          <label for="uid">User Id</label>
+
+        <div class="divider"></div>
+
+        <div class="section">
+          <button type="button" onclick="saveData(); return false;" class=" btn tooltipped waves-effect waves-light btn col s5 right" data-position="bottom" data-delay="50" data-tooltip="Click me to Save Current Information filled">Save</button>
+          <button type="button" onclick="back(); return false;" class="waves-effect tooltipped waves-light btn col s5 left" data-position="bottom" data-delay="50" data-tooltip="Click me to go back to previous panel">Back</button>
+
         </div>
       </div>
 
-       <div class="row">
-        <div class="input-field col s12">
-          <input placeholder="Location" id="location" type="text" class="validate truncate">
-          <label for="Location">Location</label>
-        </div>
-      </div>
+    </form>
+  </div>
 
 
-      <div class="row">
-        <div class="input-field col s6 ">
-          <input placeholder="lat" id="lat" type="text" class="validate truncate" >
-          
-          <label for="lat">Lat</label>
-        </div>
-        <div class="input-field col s6">
-          <input placeholder="lng" id="lng" type="text" class="validate truncate">
-          <label for="lng">Long</label>
 
 
-        </div>
 
 
-      </div>
 
 
-      <div class="row" >
-        <div class="col s6">
-          <input name="typegroup" type="radio"  value ="mosquito"  id="test1" checked />
+  <div  class="col s10  no-padding"  id="graphContainer" style="height: 400px; display:none">
+    <div class="col s11 card "  id="graph" style="height:100%">
 
-          <label for="test1">Mosquito</label>
-        </div>
-
-        <div class="col s6">
-          <input name="typegroup"  type="radio" value ="dog"  id="test2" />
-          
-          <label for="test2">Dog</label>
-        </div>
-
-
-      </div>
-
-      <div class="row">
-
-
-        <div class="col s6  blue-text  ">
-         <p style="font-size:17px">Date:</p>
-       </div>
-       <div class="col s6   blue-text text-darken-2 no-padding">
-        <input placeholder="idate" type="date" class="datepicker tooltipped center-align" data-position="bottom" data-delay="50" data-tooltip="Click here to input date of the day you got bitten" value="<?php echo date("d-M-Y"); ?>" id="idate">
-      </div>
-      
-
-      
     </div>
 
-    
+  </div>
 
-    
-    
+
+
+  <div class="col s10 no-padding ">
     <div class="row">
 
-      <div class="divider"></div>
-      
-      <div class="section">
-        <button type="button" onclick="saveData(); return false;" class=" btn tooltipped waves-effect waves-light btn col s5 right" data-position="bottom" data-delay="50" data-tooltip="Click me to Save Current Information filled">Save</button>
-        <button type="button" onclick="back(); return false;" class="waves-effect tooltipped waves-light btn col s5 left" data-position="bottom" data-delay="50" data-tooltip="Click me to go back to previous panel">Back</button>
+      <div  id = "datepanel" class="col s3" style="position:relative;z-index:1; margin-top:10px">
 
+        <div class="row card-panel  hoverable  no-padding">
+          <div class="col s5 no-padding ">
+
+            <input type="date" class="datepicker center-align  blue-text text-darken-2" value="<?php echo date('d-M-Y', strtotime("-1 months", strtotime("NOW"))); ?>" id="sdate"  style="margin:5px;border-bottom:0px;">
+          </div>
+
+
+          <div class="col s2  no-padding ">
+            <p class=" center-align text-darken-2 ">to</p>
+          </div>
+
+          <div class="col s5 no-padding">
+            <input type="date" class="datepicker center-align blue-text text-darken-2" value="<?php echo date("d-M-Y"); ?>" id="edate" placeholder="End Date" style="margin:5px;border-bottom:0px;">
+          </div>
+
+        </div>
       </div>
-    </div>
 
-  </form>
-</div>
-
+      <div class="col s10 no-padding card"  style="position:absolute;height:95%;" >
+        <div class="col s12 center   no-padding" id ="map" style="height:100%;" >
 
 
+          <script>
+
+          var map = null;
+          var marker=null;
+          var markers=[];
+          var mosImage= "{{asset('/images/marker.png')}}";
+          var mapData=null;
+          var mapDataType="mosquito";
+          var heatmap=null;
+          var state= 0;
+          var clickListener=null;
+
+          function initMap() {
+            var myLatLng = {lat: 28.54539, lng: 77.188181};
+            map = new google.maps.Map(document.getElementById('map'), {
+              zoom: 16,
+              center:myLatLng,
+              dafaultUI:false,
+              zoomControl:true,
+              rotateControl:true,
+            });
 
 
-
-
-
-<div  class="col s10  no-padding"  id="graphContainer" style="height: 400px; display:none">
-  <div class="col s11 card "  id="graph" style="height:100%">
-
-  </div>
-
-</div>
-
-
-
-<div class="col s10 no-padding ">
-  <div class="row">
-
-    <div  id = "datepanel" class="col s3" style="position:relative;z-index:1; margin-top:10px">
-
-      <div class="row card-panel  hoverable  no-padding">
-        <div class="col s5 no-padding ">
-
-          <input type="date" class="datepicker center-align  blue-text text-darken-2" value="<?php echo date('d-M-Y', strtotime("-1 months", strtotime("NOW"))); ?>" id="sdate"  style="margin:5px;border-bottom:0px;">
-        </div>
-
-
-        <div class="col s2  no-padding ">
-          <p class=" center-align text-darken-2 ">to</p>
-        </div>
-
-        <div class="col s5 no-padding">
-          <input type="date" class="datepicker center-align blue-text text-darken-2" value="<?php echo date("d-M-Y"); ?>" id="edate" placeholder="End Date" style="margin:5px;border-bottom:0px;">
-        </div>
-
-      </div>
-    </div>
-
-    <div class="col s10 no-padding card"  style="position:absolute;height:95%;" >
-      <div class="col s12 center   no-padding" id ="map" style="height:100%;" >
-
-
-        <script>
-
-        var map = null;
-        var marker=null;
-        var markers=[];
-        var mosImage= "{{asset('/images/marker.png')}}";
-        var mapData=null;
-        var mapDataType="mosquito";
-        var heatmap=null;
-        var state= 0;
-
-        function initMap() {
-          var myLatLng = {lat: 28.54539, lng: 77.188181};
-          map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 16,
-            center:myLatLng,
-            dafaultUI:false,
-            zoomControl:true,
-            rotateControl:true,
-          });
-
-
-          marker = new google.maps.Marker({
-            position: myLatLng,
-            map: map,
-            title: 'IIT Delhi',
-            draggable: true,
-          });
-        }
-
-        function getLocation() {
-          if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function(position) {
-              console.log({lat: position.coords.latitude, lng: position.coords.longitude});
-              map.setCenter({lat: position.coords.latitude, lng: position.coords.longitude});
-              marker.setPosition({lat: position.coords.latitude, lng: position.coords.longitude});
-
-            } , error,options);
+            marker = new google.maps.Marker({
+              position: myLatLng,
+              map: map,
+              title: 'IIT Delhi',
+              draggable: true,
+            });
           }
-        }
 
-        function error(err) {
-          console.warn('ERROR(' + err.code + '): ' + err.message);
-        };
+          function getLocation() {
+            if (navigator.geolocation) {
+              navigator.geolocation.getCurrentPosition(function(position) {
+                console.log({lat: position.coords.latitude, lng: position.coords.longitude});
+                map.setCenter({lat: position.coords.latitude, lng: position.coords.longitude});
+                marker.setPosition({lat: position.coords.latitude, lng: position.coords.longitude});
 
-        var options = {
-          enableHighAccuracy: true,
-          timeout: 5000,
-          maximumAge: 0
-        };
+              } , error,options);
+            }
+          }
+
+          function error(err) {
+            console.warn('ERROR(' + err.code + '): ' + err.message);
+          };
+
+          var options = {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0
+          };
 
 
 
 
-        </script>
+          </script>
 
+        </div>
       </div>
     </div>
   </div>
-</div>
 </div>  
 
 
@@ -284,6 +286,7 @@
 
 <script >
 $( document ).ready(function() {
+
 
 
 
@@ -405,6 +408,7 @@ $( document ).ready(function() {
 
 
         });
+
     </script>
 
     <script src="{{asset('/js/materialize.js')}}"></script>
@@ -474,31 +478,31 @@ $( document ).ready(function() {
 
 
 
-        }
+          }
 
-        function plotGraph(data , title)
-        {
-
-          var chart = new CanvasJS.Chart("graph",
+          function plotGraph(data , title)
           {
-            theme: "theme1",
-            title:{
-              text: title
-            },
-            animationEnabled: true,
-            axisX: {
-              valueFormatString: "MMM",
-              interval:1,
-              intervalType: "month"
 
-            },
-            axisY:{
-              includeZero: false
+            var chart = new CanvasJS.Chart("graph",
+            {
+              theme: "theme1",
+              title:{
+                text: title
+              },
+              animationEnabled: true,
+              axisX: {
+                valueFormatString: "MMM",
+                interval:1,
+                intervalType: "month"
 
-            },
-            data: [
-            {        
-              type: "line",
+              },
+              axisY:{
+                includeZero: false
+
+              },
+              data: [
+              {        
+                type: "line",
             //lineThickness: 3,        
             dataPoints: data
           }
@@ -506,32 +510,33 @@ $( document ).ready(function() {
           ]
         });
 
-          chart.render();
+            chart.render();
 
-        }
-
-
-        function getData(type) {
-          state=0;
+          }
 
 
+          function getData(type) {
+            state=0;
 
-          var sdate= moment($('#sdate').val(),'D-MMM-YYYY').format('YYYY-MM-DD');
-          var edate= moment($('#edate').val(),'D-MMM-YYYY').format('YYYY-MM-DD');
-          console.log(sdate);
-          console.log(edate);
-          var dataobj= {'type':type,'sdate':sdate,'edate':edate};
+            google.maps.event.removeListener(clickListener);
 
-          $.ajax({
-            url: '/mapdata/',
-            type: 'GET',
-            data: dataobj,
-            success: function(data) {
+            var sdate= moment($('#sdate').val(),'D-MMM-YYYY').format('YYYY-MM-DD');
+            var edate= moment($('#edate').val(),'D-MMM-YYYY').format('YYYY-MM-DD');
+            console.log(sdate);
+            console.log(edate);
+            var dataobj= {'type':type,'sdate':sdate,'edate':edate};
+
+            $.ajax({
+              url: '/mapdata/',
+              type: 'GET',
+              data: dataobj,
+              success: function(data) {
       // console.log(data);
       mapData=data;
       mapDataType=type;
       setMapOnAll(null);
       setData(data);
+
 
 
       try {
@@ -547,33 +552,36 @@ $( document ).ready(function() {
     }
 
   });
-        }
-
-        function showHeatMap(type)
-        {
-         state=2;
-         document.getElementById("map").style.display = "";
-         document.getElementById("graphContainer").style.display = "none";
-         document.getElementById("datepanel").style.display = "";
+          }
 
 
 
-         var month= {'Jan':1, 'Feb':2, 'Mar':3, 'Apr':4, 'May':5, 'Jun':6, 'Jul':7, 'Aug':8, 'Sep':9, 'Oct':10, 'Nov':11, 'Dec':12};
-         var arr1= $('#sdate').val().split("-");
-         var arr2= $('#edate').val().split("-");
 
-         arr1[1]=month[arr1[1]];
-         arr2[1]=month[arr2[1]];
-         var sdate= arr1.reverse().join("-");;
-         var edate= arr2.reverse().join("-");
+          function showHeatMap(type)
+          {
+           state=2;
+           document.getElementById("map").style.display = "";
+           document.getElementById("graphContainer").style.display = "none";
+           document.getElementById("datepanel").style.display = "";
 
-         var dataobj= {'type':type,'sdate':sdate,'edate':edate};
 
-         $.ajax({
-          url: '/mapdata/',
-          type: 'GET',
-          data: dataobj,
-          success: function(data) {
+
+           var month= {'Jan':1, 'Feb':2, 'Mar':3, 'Apr':4, 'May':5, 'Jun':6, 'Jul':7, 'Aug':8, 'Sep':9, 'Oct':10, 'Nov':11, 'Dec':12};
+           var arr1= $('#sdate').val().split("-");
+           var arr2= $('#edate').val().split("-");
+
+           arr1[1]=month[arr1[1]];
+           arr2[1]=month[arr2[1]];
+           var sdate= arr1.reverse().join("-");;
+           var edate= arr2.reverse().join("-");
+
+           var dataobj= {'type':type,'sdate':sdate,'edate':edate};
+
+           $.ajax({
+            url: '/mapdata/',
+            type: 'GET',
+            data: dataobj,
+            success: function(data) {
       // console.log(data);
       mapData=data;
       mapDataType=type;
@@ -639,9 +647,7 @@ $( document ).ready(function() {
     }
 
   }
-  $(document).ready(function() {
-    $('select').material_select();
-  });
+
 
 
   function back()
@@ -678,7 +684,7 @@ $( document ).ready(function() {
 
 
 
-        google.maps.event.addListener(map, 'click', function(event) {           
+        clickListener= google.maps.event.addListener(map, 'click', function(event) {           
  //Get the location that the user clicked.
  var clickedLocation = event.latLng;
  console.log(event);
@@ -797,5 +803,6 @@ $( document ).ready(function() {
 
 
       };
+
 
       </script>
